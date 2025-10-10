121b21492b5a8ce5771407bc8d9a38a447c955e1
yuzukq <yuzupanda1006@icloud.com>
2025-10-10 13:21:27 +0900

    不要なコンフィグを削除，シェルのasciiアートを相対パスに変更

--- laptop-windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ laptop-windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,13 @@
 oh-my-posh init pwsh | Invoke-Expression
 
-Get-Content "C:\Users\yuzup\Documents\shell_ascii_art.txt" | ForEach-Object {
+Get-Content ".\shell_ascii_art.txt" | ForEach-Object {
     Write-Host $_ -ForegroundColor Cyan
 }
 
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/easy-term.omp.json | Invoke-Expression
 
-# ドキュメントフォルダにあるアスキーアートファイルを出力する関数を定義
+# ドキュメントフォルダにあるアスキーアートファイルを出力する関数
 function Show-AsciiArt {
-    # Get-Content (エイリアス: gc, cat, type) でファイルの内容を出力
     # $Home はユーザーのホームフォルダ ($env:USERPROFILE) を示す
     Get-Content -Path "$Home\Documents\icon_ascii_art.txt"
 }
